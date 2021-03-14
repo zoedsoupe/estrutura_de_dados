575aa322d70337cf997399df18c7e9f42ef91412
Zoey <zoey.spessanha@zeetech.io>
2021-03-13 22:44:38 -0300

    LE1 exercicio2 feito

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,15 +1,16 @@
 import Test.Hspec
 
 import LE1.Exercicio1
+import LE1.Exercicio2
 
 main :: IO ()
 main = hspec $ do
   describe "testa o TAD Cilindro" $ do
-    let raio' = 3.2
+    let raio'   = 3.2
     let altura' = 1.2
-    let cil' = initCilindro(raio', altura')
+    let cil'    = initCilindro(raio', altura')
 
-    let area' = 2 * pi * raio' * (raio' + altura')
+    let area'   = 2 * pi * raio' * (raio' + altura')
     let volume' = pi * raio' * (2 * altura')
 
     describe "tenta criar um cilindro" $ do
@@ -29,3 +30,53 @@ main = hspec $ do
 
       it "deve retornar o volume correto" $
         volume cil' `shouldBe` volume'
+
+  describe "testa o TAD Conjunto de Inteiros" $ do
+    let vazio' = []
+    let xs     = [1,2,3,4]
+
+    it "deve criar um conjunto vazio" $
+      criaConjunto `shouldBe` vazio'
+
+    it "deve inserir um elemento ao onjunto" $
+      (elem 5) (insereItem 5 xs) `shouldBe` True
+
+    it "deve remover um elemento do conjunto" $
+      (elem 4) (removeItem 4 xs) `shouldBe` False
+
+    describe "testa se um elemento pertence ou não a um conjunto" $ do
+      it "deve retorna True se pertence" $
+        pertence 4 xs `shouldBe` True
+
+      it "deve retornar False se não pertence" $
+        pertence 5 xs `shouldBe` False
+
+    it "deve retornar o menor valor de um conjunto" $
+      LE1.Exercicio2.min xs `shouldBe` 1
+
+    describe "testa a união de dois conjuntos" $ do
+      let ys    = [4,5,6,1]
+      let unido = [1,2,3,4,5,6]
+
+      it "deve retornar o sejunto conjunto caso o primeiro seja vazio" $
+        uniao vazio' ys `shouldBe` ys
+
+      it "deve unir corretamente os dois conjuntos" $
+        uniao xs ys `shouldBe` unido
+
+    describe "testa se um conjunto é igual a outro" $ do
+      let ys = [1,2,3,4]
+      let zs = [4,5,6]
+
+      it "deve retornar True se os conjuntos são iguais" $
+        igual xs ys `shouldBe` True
+
+      it "deve retornar False se os conjuntos diferem-se" $
+        igual xs zs `shouldBe `False
+
+    describe "testa se um conjunto é vazio" $ do
+      it "deve retornar True se um conjunto é vazio" $
+        vazio vazio' `shouldBe` True
+
+      it "deve retornar False se o conjunto possui elementos" $
+        vazio xs `shouldBe` False
