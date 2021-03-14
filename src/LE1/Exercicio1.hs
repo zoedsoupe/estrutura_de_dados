fe5dd1616de138c047b59ca41cfb837e70114119
Zoey <zoey.spessanha@zeetech.io>
2021-03-13 21:41:56 -0300

    LE1 exercicio1 feito

---
+++ src/LE1/Exercicio1.hs
@@ -0,0 +1,50 @@
+module LE1.Exercicio1
+  ( Cilindro (..),
+    initCilindro,
+    getAltura,
+    getRaio,
+    area,
+    volume
+  ) where
+
+-- Implementação
+
+-- Record que representa um cilindro com raio e altura!
+type Altura = Double
+type Raio = Double
+
+data Cilindro = Cilindro { raio :: Raio,
+                           altura :: Altura
+                         } deriving (Show, Eq)
+
+-- Interface pública
+
+{- Recebe uma 2-Tupla ou Par, onde os 2 elemetos são Double
+   O primeiro elemento dessa 2-Tupla é o raio e o segundo, a altura
+   do Cilindro que será retornado -}
+initCilindro :: (Double, Double) -> Cilindro
+initCilindro (r, a) = Cilindro {raio = r, altura = a}
+
+{- Dado um Cilindro, retorna apenas a altura
+   Realizo uma "Correspondência de Padroões"
+   me importando apenas com que o parâmetro seja
+   um Cilindro e com sua altura -}
+getAltura :: Cilindro -> Altura
+getAltura (Cilindro _ a) = a
+
+-- Dado um Cilindro, retorno apenas o raio
+getRaio :: Cilindro -> Raio
+getRaio (Cilindro r _) = r
+
+{- Calculo a área de um Cilindro seguindo a fórmula:
+   A = 2πr^2 + h(2πr)
+   onde A = 2πr(r + a) -}
+area :: Cilindro -> Double
+area (Cilindro r a) = (*) com (r + a)
+  where com = (* pi) 2 * r
+
+{- Dado um Cilindro, calculo seu volume com a seguinte
+   fórmula: V = πr2h -}
+volume :: Cilindro -> Double
+volume (Cilindro r a) = pi * r * ((*) 2 a)
+
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,31 @@
+import Test.Hspec
+
+import LE1.Exercicio1
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "testa o TAD Cilindro" $ do
+    let raio' = 3.2
+    let altura' = 1.2
+    let cil' = initCilindro(raio', altura')
+
+    let area' = 2 * pi * raio' * (raio' + altura')
+    let volume' = pi * raio' * (2 * altura')
+
+    describe "tenta criar um cilindro" $ do
+      it "deve retornar um cilindro com os parâmetros válidos" $ do
+        initCilindro (raio', altura') `shouldBe` Cilindro {altura = altura', raio = raio'}
+
+    describe "retorna os campos básicos de um cilindro" $ do
+      it "deve retornar apenas a altura de um cilindro" $
+        getAltura cil' `shouldBe` altura'
+
+      it "deve retornar apenas o raio de um cilindro" $
+        getRaio cil' `shouldBe` raio'
+
+    describe "testa as operações de área e volume de um cilindro" $ do
+      it "deve retornar a área correta" $
+        area cil' `shouldBe` area'
+
+      it "deve retornar o volume correto" $
+        volume cil' `shouldBe` volume'
