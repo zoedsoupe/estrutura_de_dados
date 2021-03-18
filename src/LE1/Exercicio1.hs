63df5ea7c3ae79b67f042fa93402fe5d81f79eb1
Zoey <zoey.spessanha@zeetech.io>
2021-03-18 15:55:14 -0300

    refatoração do TAD Cilindro

--- src/LE1/Exercicio1.hs
+++ src/LE1/Exercicio1.hs
@@ -1,50 +1,73 @@
 module LE1.Exercicio1
-  ( Cilindro (..)
-  , initCilindro
+  ( fromTuple
   , getAltura
   , getRaio
-  , area
-  , volume
+  , calcArea
+  , calcVolume
+  , vazio
+  , isVazio
+  , valido
   ) where
 
 -- Implementação
 
--- Record que representa um cilindro com raio e altura!
+{- Um TAD que representa um Cilindro
+   que possui uma altura A e um raio R
+
+   Também defino apelidos para os tipos
+   de retorno e campos -}
 type Altura = Double
 type Raio = Double
+type Area = Double
+type Volume = Double
 
 data Cilindro = Cilindro { raio :: Raio
                          , altura :: Altura
-                         } deriving (Show, Eq)
+                         } deriving (Show, Eq, Ord)
+
+-- Contrato das implementações
+
+vazio :: Cilindro
+isVazio :: Cilindro -> Bool
+valido :: Cilindro -> Bool
+fromTuple :: (Double, Double) -> Cilindro
+getAltura :: Cilindro -> Altura
+getRaio :: Cilindro -> Raio
+calcArea :: Cilindro -> Area
+calcVolume :: Cilindro -> Volume
 
 -- Interface pública
 
-{- Recebe uma 2-Tupla ou Par, onde os 2 elemetos são Double
-   O primeiro elemento dessa 2-Tupla é o raio e o segundo, a altura
-   do Cilindro que será retornado -}
-initCilindro :: (Double, Double) -> Cilindro
-initCilindro (r, a) = Cilindro {raio = r, altura = a}
+vazio = Cilindro 0 0
+
+isVazio (Cilindro 0 0) = True
+isVazio _              = False
+
+valido (Cilindro r a)
+  | r <= 0    = False
+  | a <= 0    = False
+  | otherwise = True
 
 {- Dado um Cilindro, retorna apenas a altura
    Realizo uma "Correspondência de Padroões"
    me importando apenas com que o parâmetro seja
    um Cilindro e com sua altura -}
-getAltura :: Cilindro -> Altura
 getAltura (Cilindro _ a) = a
 
 -- Dado um Cilindro, retorno apenas o raio
-getRaio :: Cilindro -> Raio
 getRaio (Cilindro r _) = r
 
 {- Calculo a área de um Cilindro seguindo a fórmula:
    A = 2πr^2 + h(2πr)
    onde A = 2πr(r + a) -}
-area :: Cilindro -> Double
-area (Cilindro r a) = (*) com (r + a)
+calcArea (Cilindro r a) = (*) com (r + a)
   where com = (* pi) 2 * r
 
 {- Dado um Cilindro, calculo seu volume com a seguinte
    fórmula: V = πr2h -}
-volume :: Cilindro -> Double
-volume (Cilindro r a) = pi * r * ((*) 2 a)
+calcVolume (Cilindro r a) = pi * r * ((*) 2 a)
 
+{- Recebe uma 2-Tupla ou Par, onde os 2 elemetos são Double
+   O primeiro elemento dessa 2-Tupla é o raio e o segundo, a altura
+   do Cilindro que será retornado -}
+fromTuple (r, a) = Cilindro r a
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,6 +1,5 @@
 import Test.Hspec
 
-import LE1.Exercicio1
 import LE1.Exercicio2
 import LE1.Exercicio3
 import LE1.Exercicio4
@@ -17,32 +16,6 @@ criaClientes x xs
 
 main :: IO ()
 main = hspec $ do
-  describe "testa o TAD Cilindro" $ do
-    let raio'   = 3.2
-    let altura' = 1.2
-    let cil'    = initCilindro(raio', altura')
-
-    let area'   = 2 * pi * raio' * (raio' + altura')
-    let volume' = pi * raio' * (2 * altura')
-
-    describe "tenta criar um cilindro" $ do
-      it "deve retornar um cilindro com os parâmetros válidos" $ do
-        initCilindro (raio', altura') `shouldBe` Cilindro raio' altura'
-
-    describe "retorna os campos básicos de um cilindro" $ do
-      it "deve retornar apenas a altura de um cilindro" $
-        getAltura cil' `shouldBe` altura'
-
-      it "deve retornar apenas o raio de um cilindro" $
-        getRaio cil' `shouldBe` raio'
-
-    describe "testa as operações de área e volume de um cilindro" $ do
-      it "deve retornar a área correta" $
-        area cil' `shouldBe` area'
-
-      it "deve retornar o volume correto" $
-        volume cil' `shouldBe` volume'
-
   describe "testa o TAD Conjunto de Inteiros" $ do
     let vazio' = []
     let xs     = [1,2,3,4]
