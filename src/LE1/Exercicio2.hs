94efb64ed8bd8b1b4597dc77042fef10a6c9cf5f
Zoey <zoey.spessanha@zeetech.io>
2021-03-19 16:21:55 -0300

    refatoração do TAD Conjunto inteiro usando instancias de tipos

--- src/LE1/Exercicio2.hs
+++ src/LE1/Exercicio2.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE DataKinds #-}
 module LE1.Exercicio2
   ( criaConjunto
   , insereItem
@@ -7,44 +8,64 @@ module LE1.Exercicio2
   , uniao
   , igual
   , isVazio
+  , contem
+  , fromList
   ) where
 
-import Data.List (nub)
+import Data.List (nub, intercalate)
+
+{-| Interface Privada -}
+
+data ConjuntoInt = ConjuntoInt [Integer] deriving (Eq, Read, Ord)
+
+-- Instancia para ser possivel concatenar Conjuntos
+instance Semigroup ConjuntoInt where
+  (<>) (ConjuntoInt xs) (ConjuntoInt ys) = ConjuntoInt (xs ++ ys)
+
+instance Monoid ConjuntoInt where
+  mempty = ConjuntoInt []
+
+-- Instancia para imprimir de forma eprsonaliazda um Conjunto
+instance Show ConjuntoInt where
+  show (ConjuntoInt []) = "{}"
+  show (ConjuntoInt xs) = "{ " ++ intercalate "," (map (show) xs) ++ " }"
 
 -- | Contrato das interfaces
-type ConjuntoInt = [Integer]
-
 criaConjunto :: ConjuntoInt
+fromList :: [Integer] -> ConjuntoInt
 insereItem :: Integer -> ConjuntoInt -> ConjuntoInt
 removeItem :: Integer -> ConjuntoInt -> ConjuntoInt
-pertence :: Integer -> ConjuntoInt -> Bool
 minEl :: ConjuntoInt -> Integer
 uniao :: ConjuntoInt -> ConjuntoInt -> ConjuntoInt
 igual :: ConjuntoInt -> ConjuntoInt -> Bool
+contem :: ConjuntoInt -> ConjuntoInt -> Bool
 isVazio :: ConjuntoInt -> Bool
+pertence :: Integer -> ConjuntoInt -> Bool
 
 -- Inrterface Pública
 
-criaConjunto = []
+fromList xs = ConjuntoInt xs
+
+criaConjunto = mempty
 
 {- Insiro um novo elemento num conjunto
 
    Por questões de eficiência, decidi
    inserir como "head" ou "cabeçalho"
    da lista -}
-insereItem x ys = x:ys
+insereItem x (ConjuntoInt ys)
+  | not $ pertence x (ConjuntoInt ys) = ConjuntoInt (x:ys)
+  | otherwise                      = ConjuntoInt ys
 
 {-  Removo um item do conjunto de forma recursiva
     O caso base é quando chego no final da lista,
     logo, interrompo a recursão
 
     Caso seja dado um conjunto válido e um elemento x,
-    percorro o conjunto até achar um "cabeçalho" ou "y"
-    que seja igual ao x -}
-removeItem _ [] = []
-removeItem x (y:ys)
-  | x == y    = removeItem x ys
-  | otherwise = y:removeItem x ys
+    percorro o conjunto filtrando apenas os elementos
+    que são diferentes de x -}
+removeItem _ (ConjuntoInt [])     = ConjuntoInt []
+removeItem x (ConjuntoInt ys) = ConjuntoInt (filter (/= x) ys)
 
 {- Repito a lógica de remover:
    caso um elemento x seja igual a algum
@@ -53,18 +74,15 @@ removeItem x (y:ys)
 
    O caso base da recursão também previne
    argumentos errados -}
-pertence _ [] = False
-pertence x (y:ys)
-  | x == y    = True
-  | otherwise = pertence x ys
-
+pertence x (ConjuntoInt xs) = any (== x) xs
+ 
 {-  Eu poderia seguir a mesma recursividade
     das funções de "remover" ou "pertence"
     mas decidi usar uma função nativa da linguagem
     "foldl", nesse caso realiza um ação de reduzir
     um conjunto a apenas um elemento "x" onde x é
     o menor elemento do conjunto "xs" -}
-minEl xs = foldl1 (\acc x -> if x < acc then x else acc) xs
+minEl (ConjuntoInt xs) = foldl1 (\acc x -> if x < acc then x else acc) xs
 
 {-  A união de A e B é definida
     pela junção de todos os elementos de A e B,
@@ -74,19 +92,21 @@ minEl xs = foldl1 (\acc x -> if x < acc then x else acc) xs
     elementos duplicados de uma lista e também
     utilizo a função de "remover" como parâmetro
     da função "foldl" -}
-uniao xs [] = xs
-uniao [] ys = ys
-uniao xs ys =
-  xs
-    ++ ( case xs of
-           []      -> nub ys
-           (x:xs') -> foldl (flip removeItem) (removeItem x (nub ys)) xs'
-       )
+uniao xs (ConjuntoInt [])            = xs
+uniao (ConjuntoInt []) ys            = ys
+uniao (ConjuntoInt xs) (ConjuntoInt ys) =
+  (ConjuntoInt xs <> (case xs of
+                       []      -> nubbed
+                       (x:xs') -> foldl (flip removeItem) (removeItem x nubbed) xs'))
+                         where nubbed = ConjuntoInt (nub ys)
 
-igual xs ys = xs == ys
+contem (ConjuntoInt []) _ = True
+contem (ConjuntoInt (x:xs)) (ConjuntoInt ys) = elem x ys && contem (ConjuntoInt xs) (ConjuntoInt ys) 
+
+igual xs ys = contem xs ys && contem ys xs 
 
 {- Por "correspondência de valores",
    um conjunto A só será vazio caso
    A = {} -}
-isVazio [] = True
-isVazio _  = False
+isVazio (ConjuntoInt []) = True
+isVazio _                = False
