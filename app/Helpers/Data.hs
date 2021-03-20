9dd1a2422873d0c92ca4beb9ffc51e275beb4629
Zoey <zoey.spessanha@zeetech.io>
2021-03-20 03:56:00 -0300

    pequenas coreeções na CLI

--- app/Helpers/Data.hs
+++ app/Helpers/Data.hs
@@ -1,6 +1,6 @@
 module Helpers.Data (runData) where
 
-import Helpers (toInfo, toSuccess, promptLine, yellow, toBold, askUntil)
+import Helpers
 import qualified LE1.Exercicio3 as Data
 
 introData :: IO ()
@@ -11,7 +11,9 @@ introData = do
 validData :: String -> Either String String
 validData s = do
   let d = Data.fromTuple (read s :: (Int, Int, Int))
-  if Data.isInvalida d then Left "Data Inválida, tente novamente" else Right s
+  if Data.isInvalida d
+    then Left (toFailure "Data Inválida, tente novamente\n")
+    else Right s
 
 getData :: IO String
 getData = do
@@ -28,7 +30,7 @@ runData = do
   putStrLn $ toSuccess "Data criada -> " ++ toBold (show d')
   putStrLn $ toInfo "Insira uma data no formato DD/MM/AAAA:"
   d'' <- getData
-  putStrLn $ toInfo "Convertendo a string num TAD Data com os seguintes argumentos -> " ++ d'' ++ " Data " ++ (show Data.vazia)
+  putStrLn $ toInfo "Convertendo a string num TAD Data com os seguintes argumentos -> " ++ d''++ " " ++ (show Data.vazia)
   let cData = Data.converteData d'' Data.vazia
   putStrLn $ toSuccess "Data convertida -> " ++ toBold (show cData)
   let iData   = "27/07/2021"
@@ -36,7 +38,7 @@ runData = do
       sData'  = Data.somaDias (Data.converteData iData Data.vazia) 43
       iData'  = "31/12/2020"
       sData'' = Data.somaDias (Data.converteData iData' Data.vazia) 10
-  putStrLn $ toInfo "Executando função somaDias/2...\n"
+  putStrLn $ toInfo "\nExecutando função somaDias/2...\n"
   putStrLn (yellow (iData ++ " + 5 dias -> ") ++ toBold (Data.show sData))
   putStrLn (yellow (iData ++ " + 43 dias -> ") ++ toBold (Data.show sData'))
   putStrLn (yellow (iData' ++ " + 10 dias -> ") ++ toBold (Data.show sData''))
