bb4a2c7c3f75adad0363d16f9608cb5aaea68eb9
Zoey <zoey.spessanha@zeetech.io>
2021-03-22 20:48:07 -0300

    funções geradoras para LE2

---
+++ src/LE2/Helpers.hs
@@ -0,0 +1,15 @@
+module LE2.Helpers where
+
+import System.Random (Random)
+import Data.Random.Normal (mkNormals')
+
+seed :: Num a => a
+seed = 726420620519930218067873149734376981
+
+randList :: (Random a, Floating a, RealFrac a, Integral b) => (a, a) -> Int -> [b]
+randList (m, sd) n = map (round) $ take n $ mkNormals' (m, sd) seed
+
+randMatrix :: (Random a, Floating a, RealFrac a) => (a, a) -> (Int, Int) -> [[Int]]
+randMatrix (m, sd) (a, b)
+  | a /= b    = []
+  | otherwise = [ randList (m, sd) a | _ <- [1..b] ]
