0c853c01b049fe8cd1f6f3fd3c2a5e2aa8467e8e
Matheus de Souza Pessanha <matheus_pessanha2001@outlook.com>
2021-03-23 01:30:56 -0300

    melhores funções geradoras LE2, Exercicio 1 v1

---
+++ src/LE2/Exercicio1.hs
@@ -0,0 +1,14 @@
+module LE2.Exercicio1 where
+
+import LE2.Helpers (Matrix(..))
+
+somaMatriz :: Num a => Matrix a -> Matrix a -> Matrix a
+somaMatriz (Matrix _ _ [[]]) (Matrix _ _ [[]]) = Matrix 0 0 []
+somaMatriz a@(Matrix _ _ (x:xs)) b@(Matrix _ _ (y:ys))
+  | linhas a /= linhas b   = Matrix 0 0 []
+  | colunas a /= colunas b = Matrix 0 0 []
+  | otherwise              = c
+    where a' = Matrix (linhas a) (colunas a) xs
+          b' = Matrix (linhas b) (colunas b) ys
+          cs = (zipWith (+) x y : valores (somaMatriz a' b'))
+          c  = Matrix (linhas a) (colunas a) cs
