fed7c8062ab753a96bdc180be106c9bb08133803
Zoey <zoey.spessanha@zeetech.io>
2021-04-05 19:41:16 -0300

    raiz quadrada e seus respectivos testes

---
+++ src/LE1/Recursao/Raiz.hs
@@ -0,0 +1,20 @@
+module LE1.Recursao.Raiz (nSqrt) where
+
+nSqrt :: Double -> Double -> Double -> Double
+nSqrt n a i
+  | n < 0     = -1
+  | a < 0     = -1
+  | i == 0    = -1
+  | i < 0     = -1
+  | otherwise = step i (approximations a n)
+
+approximations :: Double -> Double -> [Double]
+approximations x0 n = iterate (prox n) x0
+
+prox :: Double -> Double -> Double
+prox n x0 = (x0 + n / x0) / 2
+
+step :: Double -> [Double] -> Double
+step eps (x0:t@(x1:_))
+  | abs(x0 - x1) < eps = x1
+  | otherwise          = step eps t
---
+++ test/LE1/Recursao/RaizSpec.hs
@@ -0,0 +1,29 @@
+module LE1.Recursao.RaizSpec where
+
+import Test.Hspec
+
+import LE1.Recursao.Raiz
+
+spec :: Spec
+spec = do
+  describe "testa a função de raiz quadradada" $ do
+    it "deve retornar -1 caso o número seja negativo" $ do
+      let xs = [ nSqrt (n) 2 0.01 | n <- map (negate) [1..10] ]
+      all (== -1) xs `shouldBe` True
+
+    it "deve retornar -1 caso a aproximação seja negativa" $ do
+      let xs = [ nSqrt 15 (a) 0.01 | a <- map (negate) [1..10] ]
+      all (== -1) xs `shouldBe` True
+
+    it "deve retornar -1 caso a tolerância seja negativa" $ do
+      let xs = [ nSqrt 15 7 (i) | i <- map (negate) [1..10] ]
+      all (== -1) xs `shouldBe` True
+
+    it "deve retornar -1 caso a tolerância seja 0" $ do
+      let x = nSqrt 15 7 0.0
+      (x == -1) `shouldBe` True
+
+    it "deve calcular a raiz quadrada corretamente" $ do
+      nSqrt 5 2 0.01 `shouldBe` 2.2360679779158037
+      nSqrt 4 2 0.01 `shouldBe` 2.0
+      nSqrt 3 1 0.01 `shouldBe` 1.7320508100147274
