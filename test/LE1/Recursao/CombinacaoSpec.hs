83e9d421b27f60f44f275430d0fde3555e4f698c
Matheus de Souza Pessanha <matheus_pessanha2001@outlook.com>
2021-04-05 20:51:06 -0300

    explicação e testes algoritmo combinação

---
+++ test/LE1/Recursao/CombinacaoSpec.hs
@@ -0,0 +1,31 @@
+module LE1.Recursao.CombinacaoSpec where
+
+import Test.Hspec
+
+import LE1.Recursao.Combinacao
+
+spec :: Spec
+spec = do
+  describe "testa o algoritmo de combinação" $ do
+    it "deve retornar 1 se k == 0" $ do
+      combina 2 0 `shouldBe` 1
+      combina 10 3 `shouldBe` 120      
+
+    it "deve retornar 1 se n == k" $ do
+      combina 2 2 `shouldBe` 1
+      combina 6 2 `shouldBe` 15      
+
+    it "deve retornar 0 caso k < 0" $ do
+      combina 2 (-1) `shouldBe` 0
+      combina 2 1 `shouldBe` 2
+
+    it "deve retornar 0 se k > n" $ do
+      combina 2 3 `shouldBe` 0
+      combina 20 3 `shouldBe` 1140
+
+    it "combina corretamente n e k" $ do
+      combina 20 4 `shouldBe` 4845
+      combina 15 7 `shouldBe` 6435
+      combina 49 6 `shouldBe` 13983816
+
+    
