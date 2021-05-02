e43ff86ea87787103d04fe4ee8a52af60e3f2708
Matheus de Souza Pessanha <matheus_pessanha2001@outlook.com>
2021-05-02 19:15:27 -0300

    TAD Stack adicionado com testes

---
+++ src/LE2/Stack/TAD.hs
@@ -0,0 +1,32 @@
+module LE2.Stack.TAD
+  ( new
+  , push
+  , pop
+  , isEmpty
+  , size
+  ) where
+
+data Stack a = S Integer [a] deriving (Read, Show)
+
+instance Semigroup (Stack a) where
+  (<>) (S sz1 xs) (S sz2 ys) = S (sz1 + sz2) (xs <> ys)
+
+instance Monoid (Stack a) where
+  mempty = S 0 []
+
+new :: Stack a
+new = mempty
+
+push :: Stack a -> a -> Stack a
+push (S sz xs) x = S (succ sz) (x:xs)
+
+pop :: Stack a -> Maybe (Stack a, a)
+pop (S _ [])  = Nothing
+pop (S sz xs) = Just (S (pred sz) (tail xs), head xs)
+
+isEmpty :: Stack a -> Bool
+isEmpty (S _ []) = True
+isEmpty (S _ _)  = False
+
+size :: Stack a -> Integer
+size (S sz _) = sz
---
+++ test/LE2/Stack/TADSpec.hs
@@ -0,0 +1,43 @@
+module LE2.Stack.TADSpec where
+
+import Test.Hspec
+
+import LE2.Stack.TAD
+
+spec :: Spec
+spec = do
+  describe "testa o TAD Stack" $ do
+    it "deve criar uma stack vazia corretamente" $
+      isEmpty new `shouldBe` True
+
+    it "deve inserir elementos numa stack corretamente" $ do
+      let s  = new
+      let s' = push s (2 :: Integer)
+      size s' `shouldBe` 1
+
+      let x   = new
+      let x'  = push x (10 :: Integer)
+      let x'' = push x' 15
+      size x'' `shouldBe` 2
+
+    it "deve remover elementos de uma stack corretamente" $ do
+      let s              = new
+      let s'             = push s (1 :: Integer)
+      let s''            = push s' 90
+      let Just (s''', x) = pop s''
+      x `shouldBe` 90
+      size s''' `shouldBe` 1
+
+    it "deve verificar se uma stack é vazia corretamente" $ do
+      let s   = new
+      let s2  = new
+      let s2' = push s2 "a"
+      isEmpty s `shouldBe` True
+      isEmpty s2' `shouldBe` False
+
+    it "deve calcular o tamanho de uma stack corretamente" $ do
+      let s   = new
+      let s2  = new
+      let s2' = push s2 "b"
+      size s `shouldBe` 0
+      size s2' `shouldBe` 1
