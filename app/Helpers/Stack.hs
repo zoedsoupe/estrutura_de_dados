c3a4f45a77fe049c2d32129ac31bbce9f812df4c
Matheus de Souza Pessanha <matheus_pessanha2001@outlook.com>
2021-05-08 15:46:56 -0300

    novas funções Stack, formatação, ostfixit corrigido mas não 100%

--- app/Helpers/Stack.hs
+++ app/Helpers/Stack.hs
@@ -1,6 +1,8 @@
 module Helpers.Stack where
 
+import           Data.Char                      ( isSpace )
 import           Data.List                      ( intercalate )
+import           Debug.Trace                    ( trace )
 import           Helpers
 import qualified LE2.Stack.TAD                 as Stack
 
@@ -16,7 +18,7 @@ binarize n = go n Stack.new
  where
   go 0 s = read (intercalated s) :: Integer
   go x s = go (x `div` 2) $ Stack.push s (x `mod` 2)
-  intercalated s = intercalate "" $ map show $ Stack.extract s
+  intercalated s = intercalate "" $ map show $ (Stack.<<>) s
 
 isOpenParen :: Char -> Bool
 isOpenParen '(' = True
@@ -25,6 +27,31 @@ isOpenParen _   = False
 isCloseParen :: Char -> Bool
 isCloseParen ')' = True
 isCloseParen _   = False
+
+opPriority :: Char -> Int
+opPriority '*' = 2
+opPriority '/' = 2
+opPriority '+' = 1
+opPriority '-' = 1
+opPriority _   = 0
+
+hasPriority :: Char -> Char -> Bool
+hasPriority op1 op2 | opPriority op1 >= opPriority op2 = True
+                    | otherwise                        = False
+
+isOperator :: Char -> Bool
+isOperator '+' = True
+isOperator '-' = True
+isOperator '*' = True
+isOperator '/' = True
+isOperator _   = False
+
+addSpace :: String -> String
+addSpace [] = []
+addSpace s  = (take 1 s) ++ " " ++ addSpace (drop 1 s)
+
+removeSpace :: String -> String
+removeSpace = filter $ not . isSpace
 
 parse :: String -> IO ()
 parse s = go s Stack.new
@@ -35,14 +62,34 @@ parse s = go s Stack.new
     | isOpenParen ch = go chs $ Stack.push st ch
     | isCloseParen ch = if Stack.isEmpty st
       then putStrLn $ toFailure "Erro: fecha parentêses não casa!"
-      else let Just (st', _) = Stack.pop st in go chs st'
+      else let (Just _, st') = Stack.pop st in go chs st'
     | otherwise = go chs st
 
--- É... Eu sei... Não me julgue...
+postfixit :: String -> IO String
+postfixit s = go (removeSpace s) Stack.new ""
+ where
+  go x y z | trace ("go " ++ show x ++ " " ++ show y ++ " " ++ show z) False =
+    undefined
+  go [] st pos =
+    let (remain, _) = Stack.popWhile ignore st
+    in  return . addSpace $ pos ++ remain
+  go (ch : chs) st pos
+    | isOperator ch = case Stack.peek st of
+      Nothing -> go chs (Stack.push st ch) pos
+      Just op -> if hasPriority ch op
+        then go chs (Stack.push st ch) pos
+        else
+          let (olders, st') = Stack.popWhile (hasPriority ch) st
+          in  go chs st' $ pos ++ olders
+    | otherwise = go chs st (pos ++ [ch])
+  ignore _ = True
+
+
 inversao :: IO ()
 inversao = do
+  -- É... Eu sei... Não me julgue...
   xs <- fmap reverse $ getNumbers "número> "
-  let stack = Stack.pushList Stack.new xs
+  let stack = Stack.new Stack.<>> xs
   print Stack.>- stack
   putStrLn $ toInfo "\nGostaria de inverter mais séries numéricas? (y/n)"
   res <- askUntil "resposta> " getRes
@@ -67,9 +114,20 @@ validacao = do
   putStrLn $ toInfo "Insira a cadeia de parentêses:"
   s <- getString
   parse s
-  putStrLn $ toInfo "Gostaria de validar mais alguma cadeia? (y/n)"
+  putStrLn $ toInfo "\nGostaria de validar mais alguma cadeia? (y/n)"
   res <- askUntil "resposta> " getRes
   if res == "y" then validacao else putStrLn $ toSuccess "Fim teste Stack 3!\n"
+
+posfixa :: IO ()
+posfixa = do
+  putStrLn $ toInfo "Insira a expressão a ser convertida"
+  s  <- getString
+  s' <- postfixit s
+  putStrLn . toSuccess $ "Sua expressão em notação posfixa é: " ++ s'
+  putStrLn $ toInfo "\nGostaria de converter mais alguma expressão? (y/n)"
+  res <- askUntil "resposta> " getRes
+  if res == "y" then posfixa else putStrLn $ toSuccess "Fim teste Stack 4!\n"
+
 
 runStack :: IO ()
 runStack = do
@@ -77,9 +135,9 @@ runStack = do
   putStrLn $ toInfo "Teste número 1 -> Inversão de dados!"
   putStrLn
     $ toInfo
-        "Dado uma cadeia de números, o programa imprimirá os elementos na ordem inversa a que foram inseridas"
+        "Dado uma cadeia de números, o programa imprimirá os elementos na ordem inversa a que foram inseridas!\n"
   putStrLn $ toInfo
-    "Insira um número por linha e insira \"q\" quando quiser prosseguir!\n"
+    "Insira um número por linha e insira \"q\" quando quiser prosseguir!"
   inversao
   putStrLn $ toInfo "Teste número 2 -> Conversão decimal para binário!"
   putStrLn
@@ -89,5 +147,10 @@ runStack = do
   putStrLn $ toInfo "Teste número 3 -> Validação de \"código fonte\"!"
   putStrLn
     $ toInfo
-        "Insira uma cadeia de \"(\" e \")\" para que o programa identifique se todos os pares estão corretos!"
+        "Insira uma cadeia de \"(\" e \")\" para que o programa identifique se todos os pares estão corretos!\n"
   validacao
+  putStrLn $ toInfo "Teste número 4 -> Notação infixa para posfixa!"
+  putStrLn
+    $ toInfo
+        "Dado uma expressão em notação infixa, o programa irá convertê-la para a notação posfixa!\n"
+  posfixa
--- src/LE2/Stack/TAD.hs
+++ src/LE2/Stack/TAD.hs
@@ -4,9 +4,11 @@ module LE2.Stack.TAD
   , pop
   , isEmpty
   , size
-  , pushList
+  , (<>>)
   , (>-)
-  , extract
+  , (<<>)
+  , peek
+  , popWhile
   ) where
 
 data Stack a = S Integer [a]
@@ -24,9 +26,9 @@ new = mempty
 push :: Stack a -> a -> Stack a
 push (S sz xs) x = S (succ sz) (x : xs)
 
-pop :: Stack a -> Maybe (Stack a, a)
-pop (S _  []) = Nothing
-pop (S sz xs) = Just (S (pred sz) (tail xs), head xs)
+pop :: Stack a -> (Maybe a, Stack a)
+pop st@(S _  []) = (Nothing, st)
+pop (   S sz xs) = (Just (head xs), S (pred sz) (tail xs))
 
 isEmpty :: Stack a -> Bool
 isEmpty (S _ []) = True
@@ -35,14 +37,36 @@ isEmpty (S _ _ ) = False
 size :: Stack a -> Integer
 size (S sz _) = sz
 
+peek :: Stack a -> Maybe a
+peek (S _ []     ) = Nothing
+peek (S _ (x : _)) = Just x
+
 -- | Helpers
 
-pushList :: Stack a -> [a] -> Stack a
-pushList s         []       = s
-pushList (S sz xs) (y : ys) = pushList (S (succ sz) (y : xs)) ys
+-- | Insere uma lista na Stack onde o
+-- último elemento da lista será o topo
+(<>>) :: Stack a -> [a] -> Stack a
+(<>>) s         []       = s
+(<>>) (S sz xs) (y : ys) = S (succ sz) (y : xs) <>> ys
 
+-- | Mapeia uma ação monádica numa Stack
+-- descartando o resultado
 (>-) :: (Monad m) => (a -> m b) -> Stack a -> m ()
 (>-) f (S _ xs) = mapM_ f xs
 
-extract :: Stack a -> [a]
-extract (S _ xs) = xs
+-- | Extrai os elementos
+(<<>) :: Stack a -> [a]
+(<<>) (S _ []) = []
+(<<>) (S _ xs) = extract xs []
+ where
+  extract []       zs = zs
+  extract (y : ys) zs = extract ys (y : zs)
+
+popWhile :: (a -> Bool) -> Stack a -> ([a], Stack a)
+popWhile p stack =
+  let (top, rest) = pop stack
+  in  case top of
+        Nothing -> ([], rest)
+        Just x  -> if p x
+          then let (others, rest') = popWhile p rest in (x : others, rest')
+          else ([], rest)
