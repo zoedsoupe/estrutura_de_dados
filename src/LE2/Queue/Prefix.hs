315d39a553660ee3f11662c6d2ca29334987aac1
Matheus de Souza Pessanha <matheus_pessanha2001@outlook.com>
2021-06-06 10:44:46 -0300

    eval de expr prefixa com Queue, incompleta

---
+++ src/LE2/Queue/Prefix.hs
@@ -0,0 +1,41 @@
+module LE2.Queue.Prefix where
+
+import           Debug.Trace                    ( trace )
+import qualified LE2.Queue.TAD                 as Queue
+
+data Token
+  = Op Char
+  | Num Integer
+  deriving (Show)
+
+tokenize :: String -> [Token]
+tokenize s = go (words s) []
+ where
+  go []       tokens = reverse tokens
+
+  go (x : xs) tokens = case x of
+    "+" -> go xs $ Op '+' : tokens
+    "-" -> go xs $ Op '-' : tokens
+    "*" -> go xs $ Op '*' : tokens
+    "/" -> go xs $ Op '/' : tokens
+    _   -> go xs $ Num (read x :: Integer) : tokens
+
+eval :: String -> Integer
+eval s = res . go $ fill (tokenize s) Queue.new
+ where
+  fill []       q = q
+  fill (t : ts) q = fill ts $ Queue.enq q t
+
+  go q = undefined
+
+  res q | Queue.size 1 == 1 = let (Just (Num r), _) = Queue.deq q in r
+        | otherwise         = 0
+
+  calc '+' x y = Num $ x + y
+  calc '-' x y = Num $ x - y
+  calc '*' x y = Num $ x * y
+  calc '/' x y = Num $ x `div` y
+
+ehOperador :: Token -> Bool
+ehOperador (Op  _) = True
+ehOperador (Num _) = False
